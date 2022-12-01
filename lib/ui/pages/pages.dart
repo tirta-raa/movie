7f546074208e45680cee4e08663715656c1bce56
tirta-raa <gua.koko1@gmail.com>
2022-12-01 11:59:15 +0700

    add: add new path

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -1,5 +1,7 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:movie/theme/theme.dart';
 import 'package:movie/ui/widgets/widgets.dart';
 
 part 'splash_screen_page.dart';
+part 'sign_in_page.dart';
