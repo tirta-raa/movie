3e773dbd256759d65ca03b89945de25a0ece8b74
tirta-raa <gua.koko1@gmail.com>
2022-12-01 10:02:21 +0700

    new: create theme

---
+++ lib/theme/theme.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const Color yellowColor = Color(0XFFFBD460);
+const Color greenColor = Color(0XFF3E9D9D);
+const Color blueColor = Color(0XFF503E9D);
+const Color redColor = Color(0XFFFF5C83);
+
+TextStyle textStyleMedium = GoogleFonts.raleway().copyWith(
+  color: Colors.black,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle textStyleLight = GoogleFonts.raleway().copyWith(
+  color: const Color(0XFFADADAD),
+  fontWeight: FontWeight.w300,
+);
+
+SizedBox height70 = const SizedBox(
+  height: 70,
+);
+
+SizedBox height16 = const SizedBox(
+  height: 16,
+);
+
+SizedBox height20 = const SizedBox(
+  height: 20,
+);
