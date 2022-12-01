ee8d1b563684f177f0e88af74767378ba653eab1
tirta-raa <gua.koko1@gmail.com>
2022-12-01 10:03:36 +0700

    new: create splash_screen

---
+++ lib/ui/pages/splash_screen.dart
@@ -0,0 +1,87 @@
+part of 'pages.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    imageLogo() {
+      return Image.asset(
+        'assets/logo.png',
+        width: 160,
+      );
+    }
+
+    titleText() {
+      return Text(
+        'New Experience',
+        style: textStyleMedium.copyWith(
+          fontSize: 20,
+        ),
+      );
+    }
+
+    subTitleText() {
+      return Text(
+        'Watch a new movie much \n easier than any before',
+        style: textStyleLight.copyWith(
+          fontSize: 16,
+        ),
+        textAlign: TextAlign.center,
+      );
+    }
+
+    getStartedButton() {
+      return CustomButton(
+        text: 'Get Started',
+        onPressed: () {},
+      );
+    }
+
+    signInButtonText() {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Already have an account?',
+            style: textStyleLight.copyWith(
+              fontSize: 14,
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              'Sign In',
+              style: textStyleMedium.copyWith(
+                fontSize: 14,
+                color: blueColor,
+              ),
+            ),
+          ),
+        ],
+      );
+    }
+
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              imageLogo(),
+              height70,
+              titleText(),
+              height16,
+              subTitleText(),
+              height70,
+              getStartedButton(),
+              height20,
+              signInButtonText(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
